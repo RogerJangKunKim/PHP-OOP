ed2995207de41b83c54ccd1b78d2cfe0941d1c02
Roger JangKun Kim <rb2stk@gmail.com>
2015-01-07 11:24:30 -0800

    HEYOO! TIS FOR THE LECTURE NUMERO DEUX! CHU WELCOME :D

--- Index.php
+++ Index.php
@@ -1,47 +1,77 @@
 <?php
 
 	class hair{
-		public $color = "black";
-		public $dye = "red";
-		public $clean = "wash";
+		public $color;
+		public $dye;
+		public $clean;
+
+		function __construct($color, $dye, $clean) {
+			$this->color = $color;
+			$this->dye = $dye;
+			$this->clean = $clean;
+		}
 
 		function getHair() {
 			return "{$this->color}" . "or" . "{$this->dye}";
 		}
 	}
-
-	$hair1 = new hair();
-	$hair1->color = "black";
-	$hair1->dye = "red";
+	$hair = new hair("black", "blue", "wash");
 	print "should hair be {$hair1->getHair()}?";
 
+
+
+
+
+
+
+
+
+
 	class drive{
-		public $car = "minivan";
-		public $ccolor = "blue";
-		public $brand = "hyundai";
+		public $car;
+		public $ccolor;
+		public $brand;
+
+		function __construct($car, $ccolor, $brand) {
+			$this->car = $car;
+			$this->ccolor = $ccolor;
+			$this->brand = $brand;
+		}
+
 
 		function getCar() {
 			return "{$this->ccolor}" . "and is a " . "{$this->brand}";
 		}
 	}
 
-	$car1 = new car();
-	$car1->ccolor = "blue";
-	$car1->brand = "hyundai";
+	$car1 = new car("JEEP", "BLUE", "JEEP");
 	print "My car is {$car1->getCar()}.";
 
+
+
+
+
+
+
+
+
+
 	class bike{
-		public $ride = "bike";
-		public $type = "hybrid";
-		public $bcolor = "green";
+		public $ride;
+		public $type;
+		public $bcolor;
+
+		function __construct($ride, $type, $bcolor) {
+			$this->ride = $ride;
+			$this->type = $type;
+			$this->bcolor = $bcolor;
+		}
 
 		function getBike() {
 			return "{$this->bcolor}" . "and is a " . "{$this->type}";
 		}
 	}
 
-	$bike1 = new bike();
-	$bike1->bcolor = "green";
-	$bike1->type = "hybrid";
+	$bike1 = new bike("car", "electric", "blue");
 	print "My bike is {$bike1->getBike()}.";
 ?>
