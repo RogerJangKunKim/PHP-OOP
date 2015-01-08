63bb80a0bad55d3b690a39e0b38e7924c90e522b
Roger JangKun Kim <rb2stk@gmail.com>
2015-01-08 11:03:18 -0800

    Lecture 3. is_bool, is_integer, is_null. ezpz lemon squeezy.

---
+++ run.php
@@ -0,0 +1,31 @@
+<?php
+	
+	$hello = true;
+	$age = 16;
+	$variable = null;
+
+
+
+if (is_bool($hello)===true) {
+	echo "whoa, it's a boolean.";
+	}
+else{
+	echo "tis not a boolean...BOO";
+}
+
+if (is_integer($age)===true) {
+	echo "whoa, it's an interger.";
+	}
+else{
+	echo "tis not an integer...IOO";
+}
+
+if (is_null($variable)===true) {
+	echo "whoa, it's null.";
+	}
+else{
+	echo "tis not null.";
+}
+
+	
+?>
