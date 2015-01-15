2f1dec9bee28d906e1302c2582cb64378381077f
Roger JangKun Kim <rb2stk@gmail.com>
2015-01-15 10:56:30 -0800

    classtest.php = Lecture2. run.php = lecture3. inheritance.php = lecture 4 and 5

--- classtest.php
+++ classtest.php
@@ -22,4 +22,50 @@
 	print "Poo is: ". $Poo1->getPoo();
 	print "Poo is also: ". $Poo1->getPooColor();
 
+	class Car{
+		public $type;
+		public $maker;
+		
+
+		function __construct($type, $maker) {
+			$this->type = $type;
+			$this->maker = $maker;
+			
+		}
+
+		function getCar(){
+			return $this->type;
+		}
+		function getCarMaker(){
+			return $this->maker;
+		}
+	}
+
+	$Car1 = new Car("car","Hyundai");
+	print "Car is: ". $Car1->getCar();
+	print "Car is made by: ". $Car1->getCarMaker();
+
+	class drink{
+		public $type;
+		public $name;
+		
+
+		function __construct($type, $name) {
+			$this->type = $type;
+			$this->name = $name;
+			
+		}
+
+		function getDrinkType(){
+			return $this->type;
+		}
+		function getDrinkName(){
+			return $this->name;
+		}
+	}
+
+	$Drink1 = new Drink("juice","Kool-aid");
+	print "The drink is a: ". $Drink1->getDrinkType();
+	print "The Drink is: ". $Drink1->getDrinkName();
+
 	?>
